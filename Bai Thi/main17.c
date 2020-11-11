1401c1ac33231e4c1dd10ba6967d6a4099e028ea
duongboydz1 <73330634+duongboydz1@users.noreply.github.com>
2020-11-11 15:30:11 +0700

    bai thi

---
+++ Bai Thi/main17.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+  int a[10];
+  int i, n;
+  for(i = 0;i < 10;i ++)
+  {
+  	printf("Mang a[%d]",i + 1);
+  	scanf("%d", &a[i]);
+  }
+	while(n<=0);
+          printf("\nDisplay in the reversed order:");
+
+          for(i=1; i<=10; i++) 
+		  {
+
+                    printf(" \n%d", i);
+
+          }
+}
